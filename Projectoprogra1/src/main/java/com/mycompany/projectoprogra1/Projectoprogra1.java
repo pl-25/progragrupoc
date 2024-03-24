7eb5a545e71187b16b40d24b81a4753d7da1641a
36339 <36339@DESKTOP-FJQHA5P>
2024-03-23 20:29:05 -0600

    realice la herencia

---
+++ Projectoprogra1/src/main/java/com/mycompany/projectoprogra1/Projectoprogra1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projectoprogra1;
+
+/**
+ *
+ * @author 36339
+ */
+public class Projectoprogra1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
